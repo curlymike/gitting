2e05dfef37c164d50b178f9703021719855c7cbb
Mike <curlymike@gmail.com>
2019-07-26 14:13:51 +0300

    Moving forward

--- src/main/java/tech/curly/gitting/Main2.java
+++ src/main/java/tech/curly/gitting/Main2.java
@@ -2,9 +2,13 @@ package tech.curly.gitting;
 
 /**
  * Playing with Git (2)
+ * Some more text here
  */
 
 public class Main2 {
+  // And a method here
+  public static void main(String[] args) {
 
+  }
 }
 
